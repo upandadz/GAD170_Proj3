0651c25801344d447463c58d6ef296a7b61c461f
upandadz <1033963@student.sae.edu.au>
2024-11-13 18:52:01 +1100

    integrated new input system into movement

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -4,30 +4,16 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+    void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y += 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
-
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        
         inputVector = inputVector.normalized;
         
         return inputVector;
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Speeds")]
     [SerializeField] float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
+    
+    [Header("Game Input")]
     [SerializeField] GameInput gameInput;
     
     void Update()
@@ -13,7 +17,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDirection * (moveSpeed * Time.deltaTime);
 
-        float rotateSpeed = 10f;
+        
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
     }
 }
