855045d183f0287eed406da65709cd5b27c95fb2
upandadz <1033963@student.sae.edu.au>
2024-11-13 15:01:19 +1100

    imported some assets and began work on player movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 7f;
+    
+    void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+        
+        inputVector = inputVector.normalized;
+        
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
